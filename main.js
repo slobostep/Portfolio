51e797929596275a2658f77eeca2bb428aab32db
Slobodan Radosavljević <slobostep@yahoo.com>
2020-10-04 22:18:25 +0200

    Update main.js

--- main.js
+++ main.js
@@ -5,22 +5,6 @@ var scroll = new SmoothScroll('a[href*="#"]',{
     speedAsDuration: true
 });
 
-
-
-
-// Header background change on scroll
-
-const header = document.querySelector('.header');
-window.onscroll = function(){
-    var top = window.scrollY;
-    if(top>=20){
-        header.classList.add('paint');
-    }else{
-        header.classList.remove('paint');
-    }
-}
-
-
 // click on hamburger menu
 
 const hamburger = document.getElementById('header');
@@ -29,6 +13,19 @@ const navUl = document.getElementById('container-header');
 hamburger.addEventListener('click',() =>{
     navUl.classList.toggle('active');
 })
+
+
+// GSAP animation
+
+gsap.from('.header '                , { opacity:0 , y : '-20px' , duration: .5   , delay : .5})
+gsap.from('.main-page-text h1'      , { opacity:0 , y : '-20px' , duration: .5   , delay : .7})
+gsap.from('.main-page-img'          , { opacity:0 , y : '-20px' , duration: 1.2  , delay : .7})
+gsap.from('.main-page-text h4'      , { opacity:0 , y : '-20px' , duration: .5   , delay : .9})
+gsap.from('.main-page-text hr'      , { opacity:0 , y : '-20px' , duration: .5   , delay : 1.1})
+gsap.from('.main-page-text p'       , { opacity:0 , y : '-20px' , duration: .5   , delay : 1.3})
+gsap.from('.main-page-text ul'      , { opacity:0 , y : '-20px' , duration: .5   , delay : 1.5})
+gsap.from('.buttons'                , { opacity:0 , y : '-20px' , duration: .5   , delay : 1.7})
+
 
 
 
